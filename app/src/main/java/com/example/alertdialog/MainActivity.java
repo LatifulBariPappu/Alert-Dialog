6e047babc6eeaaf8bc2213a65710e60523b5fd29
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-05-22 00:32:57 +0600

    finished

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.alertdialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,12 +22,26 @@ public class MainActivity extends AppCompatActivity {
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Clicked Button",Toast.LENGTH_LONG).show();
+                AlertDialog dialog=createDialog();
+                dialog.show();
             }
         });
     }
     AlertDialog createDialog(){
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setMessage("Do you want to do?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Clicked Yes",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("No Please", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Clicked No ",Toast.LENGTH_LONG).show();
+            }
+        });
     return builder.create();
     }
 }
